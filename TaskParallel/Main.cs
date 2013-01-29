fb52f75dd1477f19f67c0b7e680ec1021674b9c1
Fahad Sarqume <sarqume@kth.se>
2013-01-29 15:48:22 +0100

    Added funny stuff in main fuction
    Signed-off-by: Fahad Sarqume <sarqume@kth.se>

--- TaskParallel/Main.cs
+++ TaskParallel/Main.cs
@@ -7,6 +7,15 @@ namespace TaskParallel
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+            //Im adding some funny stuff here
+            DoSomeThingFunny();
+
+
 		}
+
+        private static void DoSomeThingFunny()
+        {
+            Console.WriteLine("hej hej hej");
+        }
 	}
 }
